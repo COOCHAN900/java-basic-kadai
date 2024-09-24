7c91d086fcd088b5e7ae0d35ecf1e23968c8aa09
COOCHAN900 <t.daisuke900@outlook.jp>
2024-09-24 21:25:42 +0900

    辞書クラスを更新

--- kadai_021/src/kadai_021/Dictionary_Chapter21.java
+++ kadai_021/src/kadai_021/Dictionary_Chapter21.java
@@ -1,6 +1,7 @@
 package kadai_021;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Dictionary_Chapter21 {
 	public HashMap<String, String> map;
@@ -11,7 +12,7 @@ public class Dictionary_Chapter21 {
 
 	/**
 	 * 辞書に指定の単語を追加
-	 * @param map
+	 * 繰り返し処理を使い、調べる英単語が辞書に追加されているか判定する
 	 */
 	public void addWordsInMap() {
 		map.put("apple", "りんご");
@@ -33,6 +34,21 @@ public class Dictionary_Chapter21 {
 	 */
 	public Boolean hasWord(String word) {
 		return map.containsKey(word);
+	}
+
+
+	public static void main(String[] args) {
+
+		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
+		dictionary.addWordsInMap();
+
+		for(Map.Entry<String, String> entry : dictionary.map.entrySet()) {
+			if(entry.getValue() != null) {
+				System.out.println("Key:" + entry.getKey() + " Value:" + entry.getValue());
+			} else {
+				System.out.println("辞書に存在しません");
+			}
+		}
 	}
 
 }
