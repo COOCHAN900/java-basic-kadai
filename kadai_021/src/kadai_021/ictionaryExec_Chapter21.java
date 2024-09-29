5cafcced535d7c88534ac362c808f9f3caa51174
COOCHAN900 <t.daisuke900@outlook.jp>
2024-09-29 21:47:47 +0900

    辞書実行クラスを更新

--- kadai_021/src/kadai_021/ictionaryExec_Chapter21.java
+++ kadai_021/src/kadai_021/ictionaryExec_Chapter21.java
@@ -1,22 +1,15 @@
 package kadai_021;
 
-public class ictionaryExec_Chapter21 {
+public class DictionaryExec_Chapter21 {
 
 	public static void main(String[] args) {
 		// 辞書クラスのインスタンスを生成してワードをセット
 		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
-		dictionary.addWordsInMap();
 		
 		// 配列を生成し、調べる単語をセット
-		String[] unknownWords = {"apple", "banana", "grape", "orange"};
+		String[] keys = {"apple", "banana", "grape", "orange"};
 		
-		// 検索
-		for(String word : unknownWords) {
-			if(dictionary.hasWord(word)) {
-				System.out.println(word + "の意味は" + dictionary.map.get(word));
-			} else {
-				System.out.println(word + "は辞書に存在しません");
-			}
-		}
+		// 検索実行
+		dictionary.searchWords(keys);
 	}
 }
